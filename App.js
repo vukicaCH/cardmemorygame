ce0998d93ba7216be690b0858d8887d468618880
Vuk Trifunovic <67346034+vukicaCH@users.noreply.github.com>
2022-08-11 20:19:54 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
 
-let numbers = [
+const numbers = [
   1,2,3,4,5,1,2,3,4,5
 ].sort(() => .5 - Math.random());
 
-function pickACard(num){
+const pickACard = (num) => {
   switch(num){
     case 1:
       return 'ace';
@@ -17,59 +17,34 @@ function pickACard(num){
       return 'queen';
     case 5:
       return 'king';
+    default:
+      return;
   }
 }
 
-let gameState = [];
+const gameState = numbers.map(number => ({number: number, isFlipped: false, isGuessed: false}))
 
-for(let number of numbers){
-  gameState.push({number: number, isFlipped: false, isGuessed: false});
+const checkGame = (arr) => {
+  const count = arr.filter(item => item.isFlipped).length;
+  return count === 2;
 }
 
-function checkGame(arr){
-  let count = 0;
-
-  for(let item of arr){
-    if(item.isFlipped){
-      count++;
-    }
-  }
-
-  if(count == 2){
-    return true;
-  }
-
-  return false;
-}
-
-function isGameOver(arr){
-  let num = arr.length;
-  let count = 0;
-
-  for(let item of arr){
-    if(item.isGuessed){
-      count++;
-    }
-  }
-
-  if(num === count){
-    return true;
-  }else{
-    return false;
-  }
+const isGameOver = (arr) => {
+  const num = arr.length;
+  const count = arr.filter(card => card.isGuessed).length;
+  return num === count;
 }
 
 const Card = ({number, flipped, isGuessed, flipHandler, index}) => {
 
-  let class_name = 'card' + (flipped ? ` disabled ${pickACard(number)} ` : '') + (isGuessed ? ' guessed' : '');
+  const classNameContext = 'card' + (flipped ? ` disabled ${pickACard(number)} ` : '') + (isGuessed ? ' guessed' : '');
 
   return(
     <div
-      className={class_name}
+      className={classNameContext}
       data-index={index}
       onClick={flipHandler}
-    >
-      
+    > 
     </div>
   )
 }
@@ -83,8 +58,8 @@ const Reset = ({resetHandler}) => {
 const Game = () => {
   const [cardState, setCardState] = React.useState({gameState, isClickable: true, isFinished: false, numOfTurns: 0});
 
-  function handleClick(e){
-    let newGameState = cardState.gameState.map((card, index) => {
+  const handleClick = (e) => {
+    const newGameState = cardState.gameState.map((card, index) => {
       if(index === +e.target.dataset.index){
         return {...card, isFlipped: !card.isFlipped};
       }
@@ -95,19 +70,20 @@ const Game = () => {
    setCardState({...cardState, gameState: newGameState, isClickable : checkGame(newGameState) ? false : true});
   }
 
-  function handleReset(){
-    setCardState({gameState, isClickable: true, isFinished: false, numOfTurns: 0})
+  const handleReset = () => {
+    const newGameState = gameState.sort(() => .5 - Math.random());
+    setCardState({gameState: newGameState, isClickable: true, isFinished: false, numOfTurns: 0})
   }
 
   React.useEffect(()=>{
     if(checkGame(cardState.gameState) || isGameOver(cardState.gameState)){
-      let gameIsFinished = isGameOver(cardState.gameState) ? true : false;
+      const gameIsFinished = isGameOver(cardState.gameState) ? true : false;
       if(gameIsFinished){
         setCardState({...cardState, isFinished: gameIsFinished});
         return;
       }
       setTimeout(()=>{
-        let flippedCards = cardState.gameState.filter(card => card.isFlipped);
+        const flippedCards = cardState.gameState.filter(card => card.isFlipped);
         if(flippedCards[0].number === flippedCards[1].number){
             const newGameState = cardState.gameState.map(card => {
               if(card.number === flippedCards[0].number){
@@ -116,7 +92,6 @@ const Game = () => {
                 return card;
               }
             })
-            console.log(newGameState);
             setCardState({
                 ...cardState,
                 gameState: newGameState,
@@ -128,7 +103,6 @@ const Game = () => {
           const newGameState = cardState.gameState.map(card => {
             return {...card, isFlipped: false};
           })
-          console.log(newGameState);
             setCardState(cardState => {
               return {
                 ...cardState,
@@ -145,7 +119,7 @@ const Game = () => {
   }, [cardState.gameState, cardState.isFinished]);
 
 
-  let cards = cardState.gameState.map((card, index) =>{
+  const cards = cardState.gameState.map((card, index) =>{
     return (
     <Card
       number={card.number}
